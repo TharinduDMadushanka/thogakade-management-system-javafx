9a03a7c5d46a1fc3d2579877f5dc0eab1f0438f0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-08 09:55:11 +0530

    correct ug in set UI path

--- src/edu/practice/UIController/MainViewController.java
+++ src/edu/practice/UIController/MainViewController.java
@@ -1,4 +1,20 @@
 package edu.practice.UIController;
 
+import javafx.event.ActionEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class MainViewController {
+    public AnchorPane mainContext;
+
+    public void addItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addCustomerOnAction(ActionEvent actionEvent) {
+    }
+
+    public void placeOrderOnAction(ActionEvent actionEvent) {
+    }
+
+    public void signoutOnAction(ActionEvent actionEvent) {
+    }
 }
