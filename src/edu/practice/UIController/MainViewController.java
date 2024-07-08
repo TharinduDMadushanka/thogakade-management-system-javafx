d0a876ad819850a1b7693a61803ae48588eb9f79
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-08 11:55:05 +0530

    add styles for UI

--- src/edu/practice/UIController/MainViewController.java
+++ src/edu/practice/UIController/MainViewController.java
@@ -13,7 +13,8 @@ public class MainViewController {
     public AnchorPane mainContext;
     public Button btnLogOut;
 
-    public void addItemOnAction(ActionEvent actionEvent) {
+    public void addItemOnAction(ActionEvent actionEvent) throws IOException {
+        setContext("ItemForm");
     }
 
     public void addCustomerOnAction(ActionEvent actionEvent) {
@@ -25,11 +26,21 @@ public class MainViewController {
     public void signoutOnAction(ActionEvent actionEvent) throws IOException {
         setUI("LoginForm");
     }
+
     private void setUI(String location) throws IOException {
         Stage stage = (Stage) btnLogOut.getScene().getWindow();
         stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/edu/practice/view/" + location + ".fxml"))));
         stage.show();
         stage.centerOnScreen();
+    }
+
+    private void setContext(String location) throws IOException {
+        try{
+            mainContext.getChildren().clear();
+            mainContext.getChildren().add(FXMLLoader.load(getClass().getResource("/edu/practice/view/" + location + ".fxml")));
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
