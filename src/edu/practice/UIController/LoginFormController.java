3e01bb5e09e4e57443513fa791104d04e9cf650c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-08 09:00:17 +0530

    complete DBConnection class

---
+++ src/edu/practice/UIController/LoginFormController.java
@@ -0,0 +1,18 @@
+package edu.practice.UIController;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class LoginFormController {
+    public AnchorPane context;
+    public TextField txtUserName;
+    public PasswordField txtPassword;
+    public Button btnLogin;
+
+
+    public void loginOnAction(ActionEvent actionEvent) {
+    }
+}
---
+++ src/edu/practice/db/DBConnection.java
@@ -0,0 +1,38 @@
+package edu.practice.db;
+
+import edu.practice.dto.Admin;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class DBConnection {
+    private static DBConnection dbConnection;
+    private Connection connection;
+
+    private DBConnection() throws SQLException, ClassNotFoundException {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket","root","Thariya920@");
+    }
+
+    public static DBConnection getInstance() throws SQLException, ClassNotFoundException {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+    public Connection getConnection() {
+        return connection;
+    }
+
+    public static ArrayList<Admin> userTable = new ArrayList<>();
+
+    static {
+        userTable.add(new Admin(
+                1,
+                "a",
+                "1"
+        ));
+    }
+}
