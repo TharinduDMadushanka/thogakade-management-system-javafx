d8e5e8d77ea8b3d5a64017fc09f3d0d6dfe70521
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-07-08 13:33:40 +0530

    generate attributes and methods in OrderViewController

--- src/edu/practice/UIController/OrderViewController.java
+++ src/edu/practice/UIController/OrderViewController.java
@@ -1,4 +1,34 @@
 package edu.practice.UIController;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class OrderViewController {
+    public AnchorPane orderContext;
+    public TextField txtCustomerId;
+    public TextField txtOrderId;
+    public TextField txtCustomerShow;
+    public TextField txtItemId;
+    public TextField txtItemShow;
+    public TextField txtDiscount;
+    public TextField txtQty;
+    public TableView orderTable;
+    public TableColumn colItemCode;
+    public TableColumn colQty;
+    public TableColumn colDiscount;
+
+    public void customerSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void itemSearchOnAction(ActionEvent actionEvent) {
+    }
+
+    public void addItemOnAction(ActionEvent actionEvent) {
+    }
+
+    public void placeOrderOnAction(ActionEvent actionEvent) {
+    }
 }
